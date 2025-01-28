dbb693f3299876283a3b81797ca280f12a124fac
Bernhard Bablok <bablokb@gmx.de>
2025-01-28 21:51:18 +0100

    added blueprint settings-file

---
+++ src/settings_template.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------------
+# settings_template.py: Network, hardware and application configuration
+#                       (blueprint file). Copy this file to settings.py.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/cp-webradio
+#
+# ----------------------------------------------------------------------------
+
+class Settings:
+  pass
+
+# network configuration   ----------------------------------------------------
+
+secrets  = Settings()
+secrets.ssid      = 'my_ssid'
+secrets.password  = 'my_secret_pasword'
+secrets.retries   = 3
+secrets.timeout   = 10
+
+# hardware configuration   ---------------------------------------------------
+
+from hw_config_display_audio_pack import hw_config
+
+# app configuration   --------------------------------------------------------
+
+app_config = Settings()
+app_config.debug = True
+app_config.bufsize = 16384
+app_config.max_wait = 10                         # for channel to start playing
+app_config.switch_delay = 1                      # delay playing after switch
+#app_config.autoplay = "BR Klassik"
